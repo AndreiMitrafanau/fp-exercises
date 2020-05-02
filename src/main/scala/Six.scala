03727aaf9b88975938532db41a1181c2b5e48a0a
Andrei Mitrafanau <amitrafanau@evolutiongaming.com>
2020-05-02 14:49:05 +0300

    implement methods

--- src/main/scala/Six.scala
+++ src/main/scala/Six.scala
@@ -1,4 +1,4 @@
-class Six {
+object Six {
   /**
    * Generates a random integer between 0 and Int.MaxValue (inclusive).
    * Has Int.MinValue handling
@@ -8,10 +8,57 @@ class Six {
    */
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (i, rng2) = rng.nextInt
-    if(i == Int.MinValue || i < 0) nonNegativeInt(rng2)
+    if (i == Int.MinValue || i < 0) nonNegativeInt(rng2)
     else (i, rng2)
   }
 
+  /**
+   * Generates a Double between 0 and 1, not including 1
+   *
+   * @param rng context for generating nextInt
+   * @return tuple of double between 0 and 1 exclusive and next [[RNG]]
+   */
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, rng2) = nonNegativeInt(rng)
+    (s"0.$i".toDouble, rng2)
+  }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng2) = nonNegativeInt(rng)
+    val (d, rng3) = double(rng2)
+    ((i, d), rng3)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, rng2) = double(rng)
+    val (i, rng3) = nonNegativeInt(rng2)
+    ((d, i), rng3)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
+  }
+
+  /**
+   * Generates a list of random integers.
+   *
+   * @param count Int starting from 0
+   * @param rng context for generating nextInt
+   * @return tuple with List of values and new [[RNG]] context
+   */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = count match {
+      case _ if count <= 0 => acc -> rng
+      case _ =>
+        val (i, rng2) = rng.nextInt
+        go(count - 1, rng2, i :: acc)
+    }
+
+    go(count, rng, Nil)
+  }
 }
 
 
@@ -19,7 +66,7 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 
-case class SimpleRNG(seed:Long) extends RNG {
+case class SimpleRNG(seed: Long) extends RNG {
   override def nextInt: (Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
--- src/test/scala/SixSpec.scala
+++ src/test/scala/SixSpec.scala
@@ -1,16 +1,47 @@
 import org.scalatest.freespec.AnyFreeSpec
 
 import scala.collection.mutable
+import Six._
 
 class SixSpec extends AnyFreeSpec {
+  lazy val simpleRNG = SimpleRNG(Int.MinValue)
+
   "nonNegativeInt" - {
     "handle min value" in {
-      val (number, _) = new Six().nonNegativeInt(new RNG {
+      val (number, _) = nonNegativeInt(new RNG {
         val values: mutable.Stack[Int] = mutable.Stack(Int.MinValue, 42)
+
         override def nextInt: (Int, RNG) = values.pop() -> this
       })
 
       assert(number > 0)
+    }
+  }
+  "double" - {
+    "return value between 0 and 1 exclusive" in {
+      val (number, _) = double(simpleRNG)
+      assert(number > 0)
+      assert(number < 1)
+    }
+  }
+
+  "ints" - {
+    "fill list with random ints" in {
+      val (list, _) = ints(10)(simpleRNG)
+
+      assert(list.size == 10)
+    }
+
+    "return empty list when count is 0" in {
+      val (list, _) = ints(0)(simpleRNG)
+
+      assert(list.isEmpty)
+    }
+
+    "return empty list when count is less than 0" in {
+      val (list, _) = ints(-1)(simpleRNG)
+
+      assert(list.isEmpty)
     }
   }
 }
